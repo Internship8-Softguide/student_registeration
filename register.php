67ed300cb6c77cb7399cc7d47a3563d241fd1df6
Saw Theodore <sawtheodore@gmail.com>
2024-10-22 11:53:45 +0630

    self

--- register.php
+++ register.php
@@ -1,3 +1,21 @@
+<?php
+$e_roll = $e_name = $e_age = '';
+if (isset($_POST['roll'])) {
+    $roll = $_POST["roll"];
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+
+    if ($roll === '') {
+        $e_roll = 'Roll Number must not be blank!';
+    }
+    if ($name === '') {
+        $e_name = 'Roll Number must not be blank!';
+    }
+    if ($age === '') {
+        $e_age = 'Roll Number must not be blank!';
+    }
+}
+?>
 <?php require_once ("./layout/header.php") ?>
 <h1 class="text-center my-5">Registration!</h1>
       <div class="row">
@@ -10,16 +28,13 @@
           <div class="card" style="height: 400px">
             <div class="card-body">
                 <?php if (isset($_REQUEST['success'])) { ?>
-
                     <div class='alert alert-success'><?= $_REQUEST["success"] ?></div>
-                
-                <?php } else { ?>
-                    <div class='alert alert-danger'><?= $_REQUEST["success"] ?></div>
                 <?php } ?>
-                <form action="./server.php" method="post">
+                <form action="" method="post">
                     <div class="form-group my-2">
                         <label for="roll" class="form-label">Roll Number</label>
                         <input type="text" name="roll" id="roll" class="form-control">
+                        <div class="text-danger text-sm"><?= $e_roll ?></div>
                     </div>
                     <div class="form-group my-2">
                         <label for="name" class="form-label">Name</label>
--- server.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "<pre>";
-var_dump($_POST);
-
-echo "save to database";
-
-// header("Location:./register.php?success=Save new Student!");
